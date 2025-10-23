6a8601482b8d97b80c5cdd380428fed67c26f944
onnik <onni.kivinen@gmail.com>
2025-10-23 10:52:41 +0300

    finishing assignment // TODO add docker

--- shoppingcart/src/main/java/assignment1/Main.java
+++ shoppingcart/src/main/java/assignment1/Main.java
@@ -8,22 +8,29 @@ public class Main {
 
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            ShoppingCart shoppingCart = new ShoppingCart();
             System.out.println("Select a language: en, fi, ja, sv");
             String language = sc.nextLine().toLowerCase();
             System.out.println("Select a country: US, FI, JP, SE");
             String country = sc.nextLine().toUpperCase();
             Locale myLocale = new Locale(language, country);
             ResourceBundle bundle = ResourceBundle.getBundle("localization.MessagesBundle", myLocale);
+            ShoppingCart shoppingCart = new ShoppingCart();
 
-            System.out.println(bundle.getString("count"));
-            int itemCount = sc.nextInt();
-            System.out.println(bundle.getString("price"));
-            float itemPrice = sc.nextFloat();
+            while (true) {
 
-            shoppingCart.addCartItem(itemCount, itemPrice);
-            System.out.println(bundle.getString("total") + shoppingCart.getTotalCost());
+                System.out.println(bundle.getString("count"));
+                int itemCount = sc.nextInt();
+                System.out.println(bundle.getString("price"));
+                float itemPrice = sc.nextFloat();
 
+                shoppingCart.addCartItem(itemCount, itemPrice);
+                System.out.println(bundle.getString("total") + shoppingCart.getTotalCost());
+                System.out.println(bundle.getString("continue"));
+                if (sc.next().equals("N")) {
+                    System.out.println(bundle.getString("exit"));
+                    break;
+                }
+            }
         }
     }
 }
--- shoppingcart/src/main/java/assignment1/ShoppingCart.java
+++ shoppingcart/src/main/java/assignment1/ShoppingCart.java
@@ -4,31 +4,23 @@ import java.util.ArrayList;
 
 public class ShoppingCart {
 
-    private int totalCost = 0;
+    private int totalCost;
     private final ArrayList<CartItem> cartItems;
     private int index = 1;
 
     public ShoppingCart() {
         this.cartItems = new ArrayList<>();
+        this.totalCost = 0;
     }
 
     public void addCartItem(int count, float pricePer) {
         int cost = (int) (count * pricePer);
+        totalCost += cost;
         this.cartItems.add(new CartItem(index, cost));
         index++;
     }
 
-    // public void removeCartItem(String itemName) {
-    //     for (CartItem item : cartItems) {
-    //         if (item.getName().equals(itemName)) {
-    //             cartItems.remove(item);
-    //         }
-    //     }
-    // }
     public double getTotalCost() {
-        for (CartItem item : cartItems) {
-            this.totalCost += item.getCost();
-        }
         return this.totalCost;
     }
 }
