5b4ce135645971a1d291f13aba2d3b5914ebfa56
onnik <onni.kivinen@gmail.com>
2025-10-28 11:53:46 +0200

    initialized assignment2 - shoppingcart with gui

---
+++ shoppingcartgui/src/main/java/assignment2/controller/ShoppingCartController.java
@@ -0,0 +1,5 @@
+package assignment2.controller;
+
+public class ShoppingCartController {
+
+}
---
+++ shoppingcartgui/src/main/java/assignment2/model/CartItem.java
@@ -0,0 +1,20 @@
+package assignment2.model;
+
+public class CartItem {
+
+    private final int itemName;
+    private final double itemCost;
+
+    public CartItem(int itemName, double itemCost) {
+        this.itemName = itemName;
+        this.itemCost = itemCost;
+    }
+
+    public double getCost() {
+        return itemCost;
+    }
+
+    public int getName() {
+        return itemName;
+    }
+}
